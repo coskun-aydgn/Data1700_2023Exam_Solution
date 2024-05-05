c5282cd41d31e40d13e4a762599faa43f61895b3
coskunaydogan <coskun.aydgn@gmail.com>
2024-05-05 13:06:06 +0200

    Initial commit

---
+++ src/main/java/com/example/data1700_2023exam_solution/Data17002023ExamSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.example.data1700_2023exam_solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data17002023ExamSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Data17002023ExamSolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/data1700_2023exam_solution/Data17002023ExamSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data1700_2023exam_solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data17002023ExamSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
