79c53636b8ab86ba599915632c0869da4e420e53
coskunaydgn <coskun.aydgn@gmail.com>
2024-05-08 10:57:39 +0200

    oppgave 2

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,1 +1,23 @@
-asdf
+function sendForm() {
+    const citizen={
+        firstName:$("#firstName").val(),
+        surname:$("#surname").val(),
+        DoB:$("#doB").val(),
+        SSN:$("#SSN").val(),
+        phoneNr:$("#number").val(),
+        email:$("#mail").val(),
+        city:$("#city").val(),
+        street:$("#street").val()
+    }
+    console.log(citizen)
+    if (validerFirstName() && validerSurname() && validerdoB() && validerSSN() && validerNummer()
+        && validerEpost() && validerCity() && validerStreet()){
+        $.post("/saveCitizen", citizen, function () {
+
+        })
+            .fail(function (jqXHR) {
+                const json=$.parseJSON(jqXHR.responseText);
+                $("#feil").html(json.message);
+            })
+    }
+}
