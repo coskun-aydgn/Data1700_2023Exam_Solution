38c9dcc2e6398c9cb52ac25349b57dda55fa4768
coskunaydogan <coskun.aydgn@gmail.com>
2024-05-14 09:27:07 +0200

    Oppgave - 6.2

--- src/main/java/com/example/data1700_2023exam_solution/Citizen.java
+++ src/main/java/com/example/data1700_2023exam_solution/Citizen.java
@@ -2,17 +2,9 @@ package com.example.data1700_2023exam_solution;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Generated;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Citizen {
-    @Id
-    @GeneratedValue
+
     private Integer CID;
     private String firstName;
     private String surname;
@@ -20,5 +12,91 @@ public class Citizen {
     private String SSN;
     private String phoneNr;
     private String email, city, street;
-    
+
+    public Citizen(Integer CID, String street, String city, String email,
+                   String phoneNr, String SSN, String doB, String surname,
+                   String firstName) {
+        this.CID = CID;
+        this.street = street;
+        this.city = city;
+        this.email = email;
+        this.phoneNr = phoneNr;
+        this.SSN = SSN;
+        this.doB = doB;
+        this.surname = surname;
+        this.firstName = firstName;
+    }
+    public Citizen() {}
+
+    public Integer getCID() {
+        return CID;
+    }
+
+    public void setCID(Integer CID) {
+        this.CID = CID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDoB() {
+        return doB;
+    }
+
+    public void setDoB(String doB) {
+        this.doB = doB;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getPhoneNr() {
+        return phoneNr;
+    }
+
+    public void setPhoneNr(String phoneNr) {
+        this.phoneNr = phoneNr;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
 }
